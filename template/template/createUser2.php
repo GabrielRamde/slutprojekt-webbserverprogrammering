acbdd03bd9886dd05f9fd9ff6e8640b1f74a18da
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2021-05-14 18:44:10 +0200

    Delete template/template directory

--- template/template/createUser2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$mail = filter_input(INPUT_POST,'mail', FILTER_SANITIZE_EMAIL);
-	$password = filter_input(INPUT_POST,'password', FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_LOW);
-	
-	echo $adress;
-	echo "<br>";
-	echo $password;
-?>
--- template/template/logout-template.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['anvandarnamn'] = "";
-	$_SESSION['status'] = "";
-	session_destroy();
-	header('Location: login.php'); 
-?>
